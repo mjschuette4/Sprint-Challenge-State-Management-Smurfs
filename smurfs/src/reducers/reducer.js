fb6247a6f6927d12d76b5c82bdafce8389920236
MatthewSchuette <mjschuette4@gmail.com>
2020-04-17 11:09:22 -0500

    Set up reducer/index.js

---
+++ smurfs/src/reducers/reducer.js
@@ -0,0 +1,23 @@
+
+const initialState = {
+    isLoading: false,
+    smurfs:[]
+}
+
+export const reducer = (state = initialState, action) => {
+    switch( action.type ) {
+        case GET_DATA:
+            return {
+                ...state,
+                isLoading: true
+            }
+        case LOADING_SUCCESS:
+            return {
+                ...state,
+                isLoading: false,
+                smurfs: action.payload
+            }
+        default:
+            return state;
+    }
+};
